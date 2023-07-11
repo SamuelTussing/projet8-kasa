9cdaf4eccba1f7547568465627b696237be8c88c
Tussing Samuel <sml.tussing@gmail.com>
2023-07-11 14:10:48 +0200

    footer mobile ok sass + html

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@
 import '../styles/sass/main.css';
 import Banniere from './Banniere';
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <Banniere />
+      <Footer />
     </div>
 );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import logo from '../assets/logo-kasa-blanc.png'
+
+
+function Footer(){
+    return  <div class='footer'>
+                <img src={logo} alt='logo kasa blanc' />
+                <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+}
+
+export default Footer
