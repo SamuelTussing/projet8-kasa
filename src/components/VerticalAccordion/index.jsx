93f6ad4968bcdf50323399dadd36040f20af8b3b
Tussing Samuel <sml.tussing@gmail.com>
2023-07-19 13:47:35 +0200

    création du composant accordion avec animation fade

--- src/components/VerticalAccordion/index.jsx
+++ src/components/VerticalAccordion/index.jsx
@@ -1,17 +1,32 @@
 import React, {useState} from 'react'
 import { aProposList } from '../../datas/apropos'
+import {Fade} from "../Fade"
 
 
 function Accordion(){
+    //first const define the accordion state (open/close)
+    //second const give state for css transition
     const [accordion, setActiveAccordion] = useState(-1)
+    const [open, setOpen] = useState(false)
 
-    function toggleAccordion(index){
+    
+// on click we activate selected accordion using index
+//if already active, we deactive it by setAccordion to index -1 = no content
+    function toogleAccordion(index){
         if(index === accordion){
             setActiveAccordion(-1);
+            setOpen(o => !o)
+            return
         }
         setActiveAccordion(index);
+        setOpen(o => !o)
     }
 
+    //we map the data.js to create  accordion per element. Each element is defined by id/index
+    //we listen to click to change state and play transition
+    //on click, if accordion = index then add class .active if not add noclass
+    //for picto : if accordion = index then chevron up beacause accordion is open else chevron down to suggest it can be open
+    //Fade is for css transition
     return(
         <div className='accordion'>
             <div>
@@ -19,7 +34,7 @@ function Accordion(){
             </div>
             <div className='accordion_card'>
                 {aProposList.map((item,index,title,content) =>
-                    <div key={index} onClick={() => toggleAccordion(index)}>
+                    <div key={index} onClick={() => toogleAccordion(index)}>
                         <div className='accordion_card_top'>
                             <div className='accordion_card_heading'>
                                 <h2 className={accordion=== index ? "active" : "" }>{item.title}</h2>
@@ -27,18 +42,20 @@ function Accordion(){
                             <div className='accordion_card_heading_picto'>
                                 {accordion === index ? (
                                 <>
-                                    <span className='verticle'>-</span>
+                                    <i class="fas fa-chevron-up"></i>
                                 </>
                                 ) : (
                                 <>
-                                    <span className='verticle'>+</span>
+                                    <i class="fas fa-chevron-down"></i>
                                 </>
                                 )}
                             </div>
                         </div>
-                        <div className='accordion_card_content'>
-                            <p className={accordion === index ? "active" : "inactive"}>{item.content}</p>
-                        </div>
+                        <Fade visible={open}>
+                            <div className={accordion === index ? "accordion_card_content active" : "accordion_card_content inactive"} >
+                                <p className={accordion === index ? "active" : "inactive"}>{item.content}</p>
+                            </div>
+                        </Fade>
                     </div>
                     )}
             </div>
